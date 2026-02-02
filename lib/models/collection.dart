ec92a88fa7e10a58e4edce81ed2590f333cea924
MikhTrush <uselessmash@gmail.com>
2026-02-02 20:20:31 +0700

    added isSystem property to collection

--- lib/models/collection.dart
+++ lib/models/collection.dart
@@ -3,12 +3,14 @@ class Collection {
   final String name;
   final String description;
   final List<int> songIds;
+  final bool isSystem;
 
   Collection({
     required this.id,
     required this.name,
     this.description = '',
     this.songIds = const [],
+    this.isSystem = false,
   });
 
   Map<String, dynamic> toMap() {
@@ -17,18 +19,26 @@ class Collection {
       'name': name,
       'description': description,
       'song_ids': songIds.join(','),
+      'is_system': isSystem ? 1 : 0,
     };
   }
 
   factory Collection.fromMap(Map<String, dynamic> map) {
     final songIdsStr = map['song_ids'] as String?;
-    final songIds = songIdsStr?.split(',').where((s) => s.isNotEmpty).map((s) => int.parse(s)).toList() ?? [];
+    final songIds =
+        songIdsStr
+            ?.split(',')
+            .where((s) => s.isNotEmpty)
+            .map((s) => int.parse(s))
+            .toList() ??
+        [];
 
     return Collection(
       id: map['id'],
       name: map['name'],
       description: map['description'] ?? '',
       songIds: songIds,
+      isSystem: map['is_system'] ?? false,
     );
   }
 }
